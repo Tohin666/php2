6c0f3e14839e5337b5af4b0a6dcfbf855157807b
Tohin666 <tohin666@gmail.com>
2018-10-28 07:20:51 +0300

    lesson 6 order

--- dump.sql
+++ dump.sql
@@ -11,14 +11,30 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for table myShopDB.cart
+CREATE TABLE IF NOT EXISTS `cart` (
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `sum` float DEFAULT NULL,
+  KEY `cart_users_id_fk` (`user_id`),
+  KEY `cart_products_id_fk` (`product_id`),
+  CONSTRAINT `cart_products_id_fk` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `cart_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table myShopDB.cart: ~0 rows (approximately)
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+
 -- Dumping structure for table myShopDB.categories
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Dumping data for table myShopDB.categories: ~3 rows (approximately)
+-- Dumping data for table myShopDB.categories: ~4 rows (approximately)
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
 INSERT INTO `categories` (`id`, `name`) VALUES
 	(1, 'Пластиковые автоматические печати'),
@@ -38,9 +54,9 @@ CREATE TABLE IF NOT EXISTS `orders` (
   PRIMARY KEY (`id`),
   KEY `orders_users_id_fk` (`user_id`),
   CONSTRAINT `orders_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
 
--- Dumping data for table myShopDB.orders: ~6 rows (approximately)
+-- Dumping data for table myShopDB.orders: ~8 rows (approximately)
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
 INSERT INTO `orders` (`id`, `user_id`, `fio`, `address`, `phone`, `status`, `sum`) VALUES
 	(23, 1, 'Админ Админович Админов', 'Москва, Кремль', '555-55-55', 'получен', 9705),
@@ -49,39 +65,26 @@ INSERT INTO `orders` (`id`, `user_id`, `fio`, `address`, `phone`, `status`, `sum
 	(26, 2, 'Юзер Юзерович', 'Санкт-Петербург, ул.Невская', '777-77-77', 'получен', 8685),
 	(27, 2, 'Юзер Юзерович', 'Москва, ул.Невская', '777-77-77', 'в работе', 5380),
 	(28, 5, 'Антон Тохин', 'Санкт-Петербург, ул.Невская', '222-22-22', 'получен', 4306),
-	(29, 5, 'Антон Тохин', 'Москва, ул.Невская', '222-22-22', 'удален', 784);
+	(29, 5, 'Антон Тохин', 'Москва, ул.Невская', '222-22-22', 'удален', 784),
+	(37, 20, NULL, NULL, NULL, 'новый', 10776);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 
 -- Dumping structure for table myShopDB.order_list
 CREATE TABLE IF NOT EXISTS `order_list` (
   `order_id` int(11) NOT NULL,
-  `product_id` int(11) DEFAULT NULL,
+  `product_id` int(11) NOT NULL,
   `quantity` int(11) DEFAULT NULL,
   `sum` float DEFAULT NULL,
   KEY `order_list_orders_id_fk` (`order_id`),
   CONSTRAINT `order_list_orders_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table myShopDB.order_list: ~16 rows (approximately)
+-- Dumping data for table myShopDB.order_list: ~3 rows (approximately)
 /*!40000 ALTER TABLE `order_list` DISABLE KEYS */;
 INSERT INTO `order_list` (`order_id`, `product_id`, `quantity`, `sum`) VALUES
-	(23, 1, 3, 1794),
-	(23, 3, 9, 7911),
-	(24, 2, 1, 784),
-	(24, 3, 1, 879),
-	(24, 5, 5, 3380),
-	(25, 1, 1, 598),
-	(25, 2, 1, 784),
-	(25, 3, 1, 879),
-	(25, 5, 1, 676),
-	(26, 4, 5, 8685),
-	(27, 2, 6, 4704),
-	(27, 5, 1, 676),
-	(28, 1, 2, 1196),
-	(28, 3, 2, 1758),
-	(28, 5, 2, 1352),
-	(29, 2, 1, 784),
-	(29, 2, 10, 850);
+	(37, 1, 4, 2392),
+	(37, 3, 8, 7032),
+	(37, 5, 2, 1352);
 /*!40000 ALTER TABLE `order_list` ENABLE KEYS */;
 
 -- Dumping structure for table myShopDB.photos
@@ -116,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   PRIMARY KEY (`id`),
   KEY `products_categories_id_fk` (`category_id`),
   CONSTRAINT `products_categories_id_fk` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table myShopDB.products: ~5 rows (approximately)
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
@@ -150,17 +153,18 @@ INSERT INTO `testimonials` (`id`, `name`, `testimonial`) VALUES
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `login` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `login` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
--- Dumping data for table myShopDB.users: ~3 rows (approximately)
+-- Dumping data for table myShopDB.users: ~4 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `name`, `login`, `password`) VALUES
 	(1, 'Админ', 'admin', 'admin'),
 	(2, 'Юзверь', 'user', 'qwerty'),
-	(5, 'Антон', 'tohin', 'tohin');
+	(5, 'Антон', 'tohin', 'tohin'),
+	(20, 'Гость', NULL, NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
