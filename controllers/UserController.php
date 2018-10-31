939f6a1e5fdd73ce17283f5fd22745d9b6de8d0d
Tohin666 <tohin666@gmail.com>
2018-10-31 12:04:06 +0300

    lesson7 work

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -6,6 +6,7 @@ namespace app\controllers;
 
 use app\base\App;
 use app\models\repositories\UserRepository;
+use app\models\User;
 use app\models\UserModel;
 
 class UserController extends Controller
@@ -14,17 +15,23 @@ class UserController extends Controller
     public function actionIndex()
     {
         $session = App::call()->session;
-        $user_id = $session->get('user_id');
-//        $user = (new UserRepository())->getOne($user_id);
-
         $model = [];
-        if ($user_id) {
-        // TODO if ($user_id && $user->name != 'Гость') {
-            $model = (new UserModel())->buildOrdersList($user_id);
+
+        // Проверяем что юзер залогинен, если в сессии есть айди. Если нет, то перенаправляем на авторизацию.
+        if ($user_id = $session->get('user_id')) {
+            // Получаем данные пользователя из базы.
+            $user = (new UserRepository())->getOne($user_id);
+
+            // Если учетка юзера была создана автоматически, то перенаправляем на регистрацию, чтобы доавил данные.
+            if ($user->name != 'Гость') {
+                $model = (new UserModel())->buildOrdersList($user_id);
+            } else {
+                App::call()->request->redirect('user/login');
+            }
         } else {
             App::call()->request->redirect('user/login');
         }
-
+        var_dump($model);
         echo $this->render("user", ['model' => $model]);
     }
 
@@ -42,19 +49,34 @@ class UserController extends Controller
 
                 if ($user = (new UserRepository())->getUserByLoginPass($login, $password)) {
                     $session->set('user_id', $user->id);
-                    App::call()->request->redirect('user');
+                    App::call()->request->redirect('index');
                 }
                 $model['message'] = "Неправильный логин или пароль!";
             }
 
             if ($request->post('button') == 'Зарегистрироваться') {
-
-                // Если юзер был зарегистрирован в качестве гостя, то сохраняем данные в базу под его id
-                if ($session->get('user_id')) {}
+                $user = new User();
+                $user->id = $session->get('user_id');
+                $user->name = $request->post('name');
+                $user->login = $request->post('login');
+                $user->password = $request->post('password');
+                if ($user->name && $user->login && $user->password) {
+                    // Если юзер был зарегистрирован в качестве гостя, то его данные обновятся в бд под его id,
+                    // если нет, то создастся новая запись в бд.
+                    $returnedID = (new UserRepository())->save($user);
+                    // Если пользователь создавался, то вернется сгенерированный id, если обновлялся, то вернется null
+                    if ($returnedID) {
+                        // Если пользователь создавался, то сохраняем в сессию его айди,
+                        // чтобы не выкидывало из личного кабинета.
+                        $session->set('user_id', $returnedID);
+                    }
+                    App::call()->request->redirect('index');
+                } else {
+                    // Если какое-то поле не ввели, то выводим сообщение
+                    $model['message'] = "Вы что-то забыли ввести...";
+                }
             }
         }
-
-
         echo $this->render("login", ['model' => $model]);
     }
 
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -8,7 +8,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="http://php2/style.css">
-    <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>Anton Korneev - PHP2</title>
 </head>
 <body>
