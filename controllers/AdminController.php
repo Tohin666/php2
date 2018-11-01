0fca0e96e69fd0ffa199c6cbd5f2ad955da16409
Tohin666 <tohin666@gmail.com>
2018-11-01 16:15:03 +0300

    lesson7 complete

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -3,13 +3,47 @@
 namespace app\controllers;
 
 
+use app\base\App;
+use app\models\Product;
+use app\models\repositories\ProductRepository;
+
 class AdminController extends Controller
 {
     public function actionIndex()
     {
-        $model = [];
+        $request = App::call()->request;
+        $message = '';
 
-        echo $this->render("admin", ['model' => $model]);
+        if ($request->getRequestType() == 'post') {
+            $name = $request->post('name');
+            $description = $request->post('description');
+            $price = $request->post('price');
+
+//            $photo = uploadFile(PUBLIC_DIR . 'img/', 'loadedImage');
+
+            if ($name && $description && $price) {
+                $product = new Product();
+                $product->name = $name;
+                $product->description = $description;
+                $product->price = $price;
+
+                (new ProductRepository())->save($product);
+                App::call()->request->redirect('admin');
+
+            } else {
+                $message = 'Вы что-то забыли ввести...';
+            }
+        }
+
+        if ($request->getRequestType() == 'get') {
+            $deleteProduct = (new ProductRepository())->getOne($request->get('id'));
+            (new ProductRepository())->delete($deleteProduct);
+            App::call()->request->redirect('admin');
+        }
+
+        $model = (new ProductRepository())->getAll();
+
+        echo $this->render("admin", ['model' => $model, 'message' => $message]);
     }
 
 }
